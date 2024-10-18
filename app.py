512e8267ba76dd063129217cc5a11a09a1a78f30
zwf <zwfxiaxue@163.com>
2024-10-18 18:01:16 +0800

    add job apply & html page

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
-from flask import Flask, render_template, Response, jsonify
-from database.jobs import get_jobs
+from flask import Flask, render_template, Response, jsonify, request
+from database.jobs import get_job_list, get_job_info_by_id
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    jobs = get_jobs()
+    jobs = get_job_list()
     return render_template('home.html', jobs=jobs)
 
 
 @app.route('/info')
 def info():
-    jobs = get_jobs()
+    jobs = get_job_list()
     return jsonify(jobs)
+
+
+@app.route('/job/<job_id>')
+def job(job_id):
+    job_info = get_job_info_by_id(job_id)
+    if not job_info:
+        return "Job not found", 404
+    return render_template('job_page.html', job=job_info)
+
+
+@app.route('/job/<job_id>/apply', methods=['POST'])
+def job_apply(job_id):
+    data = request.form
+    file = request.files.get('file')
+    print(data, file)
+    if not file:
+        return "File not loaded", 400
+
+    file.save(f"uploads/{file.filename}")
+
+    return render_template('job_apply', data=data, file=file.filename)
 
 
 if __name__ == "__main__":
--- database/jobs.py
+++ database/jobs.py
@@ -5,15 +5,36 @@ mysql_url = get_config().get('mysql_url')
 
 
 def get_engine():
-    return create_engine(mysql_url)
+    try:
+        return create_engine(mysql_url)
+    except Exception as e:
+        print(f"Error connecting to database: {e}")
+        return None
 
 
 engine = get_engine()
 
 
-def get_jobs():
+def get_job_list():
+    if not engine:
+        return None
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs"))
         data = result.fetchall()
         print(data)
+        print(type(data))
     return data
+
+
+def get_job_info_by_id(job_id):
+    if not engine:
+        return None
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs WHERE id = :job_id"), {'job_id': job_id})
+        data = result.fetchone()
+        if not data:
+            print("Job not found")
+            return None
+        job_info = data._asdict()
+
+    return job_info
